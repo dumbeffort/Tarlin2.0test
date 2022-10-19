bd25d78ff3c5f394e5e92729058e0642517c8eba
Dago <dago.gav@gmail.com>
2022-10-19 15:42:01 +0200

    docs updated

--- Marlin/Marlin.ino
+++ Marlin/Marlin.ino
@@ -3,33 +3,7 @@
   Marlin Firmware
 
   (c) 2011-2020 MarlinFirmware
-  Portions of Marlin are (c) by their respective authors.
-  All code complies with GPLv2 and/or GPLv3
-
-================================================================================
-
-Greetings! Thank you for choosing Marlin 2 as your 3D printer firmware.
-
-To configure Marlin you must edit Configuration.h and Configuration_adv.h
-located in the root 'Marlin' folder. Check our Configurations repository to
-see if there's a more suitable starting-point for your specific hardware.
-
-Before diving in, we recommend the following essential links:
-
-Marlin Firmware Official Website
-
-  - https://marlinfw.org/
-    The official Marlin Firmware website contains the most up-to-date
-    documentation. Contributions are always welcome!
-
-Configuration
-
-  - https://github.com/MarlinFirmware/Configurations
-    Example configurations for several printer models.
-
-  - https://www.youtube.com/watch?v=3gwWVFtdg-4
-    A good 20-minute overview of Marlin configuration by Tom Sanladerer.
-    (Applies to Marlin 1.0.x, so Jerk and Acceleration should be halved.)
+  Portion Acceleration should be halved.)
     Also... https://www.google.com/search?tbs=vid%3A1&q=configure+marlin
 
   - https://marlinfw.org/docs/configuration/configuration.html
