3c9bdfa92d20a3b5d8a3188f888285066d9dc6cc
Dago <dago.gav@gmail.com>
2022-10-19 15:37:21 +0200

    Update Configuration_adv.h
    removed thermocouple

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -21,35 +21,6 @@
  */
 #pragma once
 
-/**
- * Configuration_adv.h
- *
- * Advanced settings.
- * Only change these if you know exactly what you're doing.
- * Some of these settings can damage your printer if improperly set!
- *
- * Basic settings can be found in Configuration.h
- */
-#define CONFIGURATION_ADV_H_VERSION 020007
-
-// @section temperature
-
-//===========================================================================
-//============================= Thermal Settings ============================
-//===========================================================================
-
-/**
- * Thermocouple sensors are quite sensitive to noise.  Any noise induced in
- * the sensor wires, such as by stepper motor wires run in parallel to them,
- * may result in the thermocouple sensor reporting spurious errors.  This
- * value is the number of errors which can occur in a row before the error
- * is reported.  This allows us to ignore intermittent error conditions while
- * still detecting an actual failure, which should result in a continuous
- * stream of errors from the sensor.
- *
- * Set this value to 0 to fail on the first error to occur.
- */
-#define THERMOCOUPLE_MAX_ERRORS 15
 
 //
 // Custom Thermistor 1000 parameters
